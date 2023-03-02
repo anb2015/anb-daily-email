4a620c084575176ac5f202eaff5e05d11481937a
anb2015 <92055455+anb2015@users.noreply.github.com>
2023-03-02 10:21:21 -0500

    Delete joke.py

--- joke.py
+++
@@ -1,8 +0,0 @@
-# Retrieves formatted joke from https://sv443.net/jokeapi/v2/
-import requests
-
-JOKEAPI_URL = "https://v2.jokeapi.dev/joke/Miscellaneous,Pun?blacklistFlags=nsfw,racist,sexist," \
-              "explicit&format=txt&type=single?idRange=1-1368"
-
-response = requests.get(url=JOKEAPI_URL)
-joke_text = f"Joke of the Day:\n\n{response.text}"
